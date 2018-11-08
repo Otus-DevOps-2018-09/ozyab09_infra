9ccda46f62d610f7ef5d40531193ff9df0e707db
Vyacheslav Egorov <tjs05@mail.ru>
2018-11-08 22:50:47 +0300

    HW-10 Ansible-3

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -9,3 +9,15 @@ resource "google_compute_firewall" "firewall_ssh" {
 
   source_ranges = ["${var.source_ranges}"]
 }
+
+resource "google_compute_firewall" "firewall_http" {
+  name    = "allow-http"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["${var.source_ranges}"]
+}
